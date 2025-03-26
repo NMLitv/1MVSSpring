d8e65d73895c48911d3ff0260f0fa87167d4dbba
Nikita Litvinov <litvinovnikita97@mail.ru>
2025-03-27 02:35:40 +0300

    twentyFreeLesson

---
+++ src/main/java/ru/alishev/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,38 @@
+package ru.alishev.springcourse.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        FilterRegistration.Dynamic hiddenHttpMethodFilter = aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
+        hiddenHttpMethodFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
+
---
+++ src/main/java/ru/alishev/springcourse/config/models/Person.java
@@ -0,0 +1,4 @@
+package ru.alishev.springcourse.config.models;
+
+public class People {
+}
