6ef79319acdff94a45912364326a06c74fe8f289
Nikita Litvinov <litvinovnikita97@mail.ru>
2025-03-29 00:09:56 +0300

    twentyFourLesson

--- src/main/java/ru/alishev/springcourse/config/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/config/dao/PersonDAO.java
@@ -17,10 +17,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24 , "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 43,  "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 34,  "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34,  "katy@mail.ru"));
     }
 
     public List<Person> index() {
